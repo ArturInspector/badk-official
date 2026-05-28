cad0de6ae83813fe3005e24710e38735b588e617
ArturInspector <bikk_artur@mail.ru>
2026-05-28 14:35:57 +0600

    feat(i18n): auto-reload gunicorn workers on Rosetta save via SIGHUP, persist locale via bind mount

--- apps/core/apps.py
+++ apps/core/apps.py
@@ -1,3 +1,6 @@
+import os
+import signal
+
 from django.apps import AppConfig
 
 
@@ -5,3 +8,15 @@ class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.core'
     verbose_name = 'Прочее'
+
+    def ready(self):
+        from rosetta.signals import post_save as rosetta_post_save
+        rosetta_post_save.connect(_reload_gunicorn)
+
+
+def _reload_gunicorn(sender, **kwargs):
+    try:
+        with open('/tmp/gunicorn.pid') as f:
+            os.kill(int(f.read().strip()), signal.SIGHUP)
+    except Exception:
+        pass
